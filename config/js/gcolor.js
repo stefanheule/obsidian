df83d3c0e0d25581bdf13b12c9836ec3c6ae2903
Stefan Heule <stefanheule@gmail.com>
2015-08-29 21:22:47 -0700

    work in progress

--- config/js/gcolor.js
+++ config/js/gcolor.js
@@ -102,6 +102,7 @@ var GColor = (function () {
   // Convert a hex color (e.g. FFFFFF) into a GColor8 value used by the
   // Pebble SDK.
   function GColorFromHex(hex) {
+    console.log(hex);
     var hexNum = parseInt(hex, 16);
     var a = 192;
     var r = (((hexNum >> 16) & 0xFF) >> 6) << 4;
