9f97d0f517f814c6c791a0bb29adc089d46144d9
Stefan Heule <stefanheule@gmail.com>
2015-11-21 17:43:26 -0800

    update screenshot processing script

--- scripts/assemble_screenshots.sh
+++ scripts/assemble_screenshots.sh
@@ -18,16 +18,16 @@ textx2=$((8+x2))
 textx3=$((8+x3))
 texty=$((190+border))
 
-for prefix in "" "aplite_"; do
+for platform in "aplite" "basalt" "chalk"; do
   # low battery look
   convert data/white.png -background white -resize ${width}x${height}! tmp.png
-  composite -compose atop -geometry +$x1+$border screenshots/${prefix}battery-low-1.png tmp.png tmp.png
-  composite -compose atop -geometry +$x2+$border screenshots/${prefix}battery-low-2.png tmp.png tmp.png
-  composite -compose atop -geometry +$x3+$border screenshots/${prefix}battery-low-3.png tmp.png tmp.png
+  composite -compose atop -geometry +$x1+$border screenshots/$platform/battery-low-1.png tmp.png tmp.png
+  composite -compose atop -geometry +$x2+$border screenshots/$platform/battery-low-2.png tmp.png tmp.png
+  composite -compose atop -geometry +$x3+$border screenshots/$platform/battery-low-3.png tmp.png tmp.png
   convert tmp.png -pointsize 15 -font /usr/share/fonts/truetype/msttcorefonts/verdana.ttf -annotate +$textx1+$texty '30%% battery left' tmp.png
   convert tmp.png -pointsize 15 -font /usr/share/fonts/truetype/msttcorefonts/verdana.ttf -annotate +$textx2+$texty '20%% battery left' tmp.png
   convert tmp.png -pointsize 15 -font /usr/share/fonts/truetype/msttcorefonts/verdana.ttf -annotate +$textx3+$texty '10%% battery left' tmp.png
-  mv tmp.png screenshots/${prefix}battery.png
+  mv tmp.png screenshots/$platform/battery.png
 
   textx1=$((12+x1))
   textx2=$((-5+x2))
@@ -35,13 +35,13 @@ for prefix in "" "aplite_"; do
 
   # bluetooth
   convert data/white.png -background white -resize ${width}x${height}! tmp.png
-  composite -compose atop -geometry +$x1+$border screenshots/${prefix}bluetooth-popup-2.png tmp.png tmp.png
-  composite -compose atop -geometry +$x2+$border screenshots/${prefix}bluetooth-popup-1.png tmp.png tmp.png
-  composite -compose atop -geometry +$x3+$border screenshots/${prefix}bluetooth-icon.png tmp.png tmp.png
+  composite -compose atop -geometry +$x1+$border screenshots/$platform/bluetooth-popup-2.png tmp.png tmp.png
+  composite -compose atop -geometry +$x2+$border screenshots/$platform/bluetooth-popup-1.png tmp.png tmp.png
+  composite -compose atop -geometry +$x3+$border screenshots/$platform/bluetooth-icon.png tmp.png tmp.png
   convert tmp.png -pointsize 15 -font /usr/share/fonts/truetype/msttcorefonts/verdana.ttf -annotate +$textx1+$texty 'Connection lost' tmp.png
   convert tmp.png -pointsize 15 -font /usr/share/fonts/truetype/msttcorefonts/verdana.ttf -annotate +$textx2+$texty 'Connection regained' tmp.png
   convert tmp.png -pointsize 15 -font /usr/share/fonts/truetype/msttcorefonts/verdana.ttf -annotate +$textx3+$texty 'No connection' tmp.png
-  mv tmp.png screenshots/${prefix}bluetooth.png
+  mv tmp.png screenshots/$platform/bluetooth.png
 
   textx1=$((12+x1))
   textx2=$((10+x2))
@@ -49,37 +49,39 @@ for prefix in "" "aplite_"; do
 
   # date
   convert data/white.png -background white -resize ${width}x${height}! tmp.png
-  composite -compose atop -geometry +$x1+$border screenshots/${prefix}main.png tmp.png tmp.png
-  composite -compose atop -geometry +$x2+$border screenshots/${prefix}date-1.png tmp.png tmp.png
-  composite -compose atop -geometry +$x3+$border screenshots/${prefix}date-2.png tmp.png tmp.png
+  composite -compose atop -geometry +$x1+$border screenshots/$platform/main.png tmp.png tmp.png
+  composite -compose atop -geometry +$x2+$border screenshots/$platform/date-1.png tmp.png tmp.png
+  composite -compose atop -geometry +$x3+$border screenshots/$platform/date-2.png tmp.png tmp.png
   convert tmp.png -pointsize 15 -font /usr/share/fonts/truetype/msttcorefonts/verdana.ttf -annotate +$textx1+$texty 'Normal position' tmp.png
   convert tmp.png -pointsize 15 -font /usr/share/fonts/truetype/msttcorefonts/verdana.ttf -annotate +$textx2+$texty 'Date moved left' tmp.png
   convert tmp.png -pointsize 15 -font /usr/share/fonts/truetype/msttcorefonts/verdana.ttf -annotate +$textx3+$texty 'Date moved right' tmp.png
-  mv tmp.png screenshots/${prefix}date.png
+  mv tmp.png screenshots/$platform/date.png
 
   # main
   convert data/white.png -background white -resize ${width}x${normalheight}! tmp.png
-  composite -compose atop -geometry +$x1+$border screenshots/${prefix}main.png tmp.png tmp.png
-  composite -compose atop -geometry +$x2+$border screenshots/${prefix}theme-1.png tmp.png tmp.png
-  composite -compose atop -geometry +$x3+$border screenshots/${prefix}theme-2.png tmp.png tmp.png
-  mv tmp.png screenshots/${prefix}overview.png
+  composite -compose atop -geometry +$x1+$border screenshots/$platform/main.png tmp.png tmp.png
+  composite -compose atop -geometry +$x2+$border screenshots/$platform/theme-1.png tmp.png tmp.png
+  composite -compose atop -geometry +$x3+$border screenshots/$platform/theme-2.png tmp.png tmp.png
+  mv tmp.png screenshots/$platform/overview.png
 
   # themes
   convert data/white.png -background white -resize ${width}x${normalheight}! tmp.png
-  composite -compose atop -geometry +$x1+$border screenshots/${prefix}theme-6.png tmp.png tmp.png
-  composite -compose atop -geometry +$x2+$border screenshots/${prefix}theme-4.png tmp.png tmp.png
-  composite -compose atop -geometry +$x3+$border screenshots/${prefix}theme-3.png tmp.png tmp.png
-  mv tmp.png screenshots/${prefix}theme-overview-1.png
+  composite -compose atop -geometry +$x1+$border screenshots/$platform/theme-6.png tmp.png tmp.png
+  composite -compose atop -geometry +$x2+$border screenshots/$platform/theme-4.png tmp.png tmp.png
+  composite -compose atop -geometry +$x3+$border screenshots/$platform/theme-3.png tmp.png tmp.png
+  mv tmp.png screenshots/$platform/theme-overview-1.png
 
   # themes
   convert data/white.png -background white -resize ${width}x${normalheight}! tmp.png
-  composite -compose atop -geometry +$x1+$border screenshots/${prefix}theme-5.png tmp.png tmp.png
-  composite -compose atop -geometry +$x2+$border screenshots/${prefix}theme-7.png tmp.png tmp.png
-  composite -compose atop -geometry +$x3+$border screenshots/${prefix}theme-8.png tmp.png tmp.png
-  mv tmp.png screenshots/${prefix}theme-overview-2.png
+  composite -compose atop -geometry +$x1+$border screenshots/$platform/theme-5.png tmp.png tmp.png
+  composite -compose atop -geometry +$x2+$border screenshots/$platform/theme-7.png tmp.png tmp.png
+  composite -compose atop -geometry +$x3+$border screenshots/$platform/theme-8.png tmp.png tmp.png
+  mv tmp.png screenshots/$platform/theme-overview-2.png
 done
 
-for f in screenshots/*.png; do
-  pngcrush -rem time $f tmp.png &> /dev/null
-  mv tmp.png $f
+for platform in "aplite" "basalt" "chalk"; do
+  for f in screenshots/$platform/*.png; do
+    pngcrush -rem time $f tmp.png &> /dev/null
+    mv tmp.png $f
+  done
 done
