6ae96d0a3c6780bdff6544657d81efa7989bf73e
Stefan Heule <stefanheule@gmail.com>
2016-09-22 16:23:29 -0700

    make script a bit nicer

--- src/scripts/updated_config.sh
+++ src/scripts/updated_config.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "IMPORTANT: Make sure you updated the config branch and pushed to github, otherwise this might not do anything."
+
 version=$(cat src/js/pebble-js-app.js |& grep "stefanheule.com/obsidian/config/" | sed "s/.*config\/\([0-9]*\).*/\1/")
 
+echo "Detected config version $version..."
+echo "Resetting config $version on server."
 ssh linode "sudo rm -rf /home/stefan/www/pages/common/data/obsidian/config/$version"
+echo "Done :)"
